722aa5778c427806afccea877bf7a71c79d765b7
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-04-03 11:25:20 +0530

    Update Set.java

--- src/test/java/SetupClass/Set.java
+++ src/test/java/SetupClass/Set.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.firefox.FirefoxOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Set {
